831c7a91c7a35172237351e6d0d2610f1054c8c3
edndacomputer <the-grateful-ed@riseup.net>
2022-05-27 02:08:08 -0700

    prod

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5ecBa2a6E31a6AF373f926e566634ce2A6FaBd41";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-nijt6pffn-the-grateful-ed.vercel.app//api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
